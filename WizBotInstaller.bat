aeed389989e26a365ed57f4e6322484ffa64e600
Richard Morales <wizkiller96@hotmail.com>
2017-03-08 02:33:56 -0500

    Update WizBotInstaller.bat

--- WizBotInstaller.bat
+++ WizBotInstaller.bat
@@ -69,10 +69,30 @@ CALL WizBotAutoRun.bat
 GOTO End
 
 :ffmpeg
+goto check_Permissions
 IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
 
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+	    echo.
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+		        echo.
+		        pause
+    ) else (
+        echo Failure: Current permissions inadequate.
+		          echo.
+		          echo Run again as Administrator.
+		          echo.
+		          pause >nul
+		          goto exit
+    )
+
 :64BIT
-TITLE WizBot FFMPEG Installer for 64bit OS! 
+TITLE WizBot FFMPEG Installer for 64bit OS!
+ECHO. 
 ECHO Welcome to WizBot FFMPEG Installer! 
 ECHO.
 ECHO Installing ffmpeg in "%SystemDrive%\ffmpeg\"...
@@ -93,6 +113,8 @@ ECHO ffmpeg zip downloaded: %FILENAME%...
 ECHO.
 ECHO Press any key to continue extraction...
 pause >nul 2>&1
+ECHO.
+ECHO Extracting files...
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170111-e71b811-win64-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170111-e71b811-win64-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg.zip" powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%SystemDrive%\ffmpeg\ffmpeg.zip"', '%SystemDrive%\ffmpeg\'); }"
@@ -102,20 +124,25 @@ ECHO.
 pause
 ECHO.
 ECHO Backing up System PATH registry to "%SystemDrive%\ffmpeg\path_registry_backup.reg"
-ECHO NOTE: If you get a prompt to replace the registry file "path_registry_backup.reg", Choose "No"!
+ECHO NOTE: If you get a prompt to replace the registry file "path_registry_backup.reg"
+ECHO Choose "No"!
 ECHO.
 pause
 reg export "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" "%SystemDrive%\ffmpeg\path_registry_backup.reg"
 ECHO Registry file backup complete!
+@echo on
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v "path" /t REG_SZ /d "%path%;%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win64-static\bin"
+@echo off
 ECHO ffmpeg path has been set!
 ECHO.
-ECHO ffmpeg Installation complete!
-pause
-GOTO End
+ECHO ffmpeg Installation complete! (Restarting WizBotInstaller.bat is required)
+ECHO Press any key to exit...
+pause >nul 2>&1
+GOTO exit
 
 :32BIT
-TITLE WizBot FFMPEG Installer for 32bit OS! 
+TITLE WizBot FFMPEG Installer for 32bit OS!
+ECHO. 
 ECHO Welcome to WizBot FFMPEG Installer! 
 ECHO.
 ECHO Installing ffmpeg in "%SystemDrive%\ffmpeg\"...
@@ -136,6 +163,8 @@ ECHO ffmpeg zip downloaded: '%FILENAME%'...
 ECHO.
 ECHO Press any key to continue extraction...
 pause >nul 2>&1
+ECHO.
+ECHO Extracting files...
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg-20170125-2080bc3-win32-static" RD /S /Q "%SystemDrive%\ffmpeg\ffmpeg-20170125-2080bc3-win32-static"
 IF EXIST "%SystemDrive%\ffmpeg\ffmpeg.zip" powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%SystemDrive%\ffmpeg\ffmpeg.zip"', '%SystemDrive%\ffmpeg\'); }"
@@ -145,17 +174,21 @@ ECHO.
 pause
 ECHO.
 ECHO Backing up System PATH registry to "%SystemDrive%\ffmpeg\path_registry_backup.reg"
-ECHO NOTE: If you get a prompt to replace the registry file "path_registry_backup.reg", Choose "No"!
+ECHO NOTE: If you get a prompt to replace the registry file "path_registry_backup.reg"
+ECHO Choose "No"!
 ECHO.
 pause
 reg export "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" "%SystemDrive%\ffmpeg\path_registry_backup.reg"
 ECHO Registry file backup complete!
+@echo on
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v "path" /t REG_SZ /d "%path%;%SystemDrive%\ffmpeg\ffmpeg-20170225-7e4f32f-win32-static\bin"
+@echo off
 ECHO ffmpeg path has been set!
 ECHO.
-ECHO ffmpeg Installation complete!
-pause
-GOTO End
+ECHO ffmpeg Installation complete! (Restarting WizBotInstaller.bat is required)
+ECHO Press any key to exit...
+pause >nul 2>&1
+GOTO exit
 
 :End
 cls
